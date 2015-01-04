0d942d0ae410e6803f19c47020ef99d6084a060a
jsjiang <jsjiang@tencent.com>
2015-01-04 16:26:59 +0800

    --------

--- plugins/wordpress/src/org/wikiup/plugins/wordpress/WordPressServletProcessor.java
+++ plugins/wordpress/src/org/wikiup/plugins/wordpress/WordPressServletProcessor.java
@@ -3,6 +3,7 @@ package org.wikiup.plugins.wordpress;
 import org.wikiup.core.util.FileUtil;
 import org.wikiup.core.util.StringUtil;
 import org.wikiup.servlet.ServletProcessorContext;
+import org.wikiup.servlet.beans.ServletContextContainer;
 import org.wikiup.servlet.inf.ServletProcessor;
 
 import javax.servlet.RequestDispatcher;
@@ -13,7 +14,7 @@ public class WordPressServletProcessor implements ServletProcessor {
     public void process(ServletProcessorContext context) {
         String uri = StringUtil.connect("", context.getRequestURI(), '/');
         String index = StringUtil.connect(uri, "index.php", '/');
-        if(FileUtil.isExists(context.getRealPathByURI(index))) {
+        if(FileUtil.isExists(ServletContextContainer.getInstance().getRealPath(index))) {
             RequestDispatcher dispatcher = context.getServletRequest().getRequestDispatcher(index);
             try {
                 dispatcher.forward(context.getServletRequest(), context.getServletResponse());
