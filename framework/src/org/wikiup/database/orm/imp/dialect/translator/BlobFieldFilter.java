75f0b23e60762bf99e188f5fe1507b7fc78a0be8
jsjiang <jsjiang@tencent.com>
2015-01-06 18:57:17 +0800

    --------

---
+++ framework/src/org/wikiup/database/orm/imp/dialect/translator/BlobFieldFilter.java
@@ -0,0 +1,22 @@
+package org.wikiup.database.orm.imp.dialect.translator;
+
+import org.wikiup.core.inf.Translator;
+import org.wikiup.database.util.BytesBlob;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.Blob;
+
+public class BlobFieldFilter implements Translator<Object, Blob> {
+    public Blob translate(Object object) {
+        byte[] bytes = null;
+        if(object instanceof byte[])
+            bytes = (byte[]) object;
+        else if(object instanceof String) {
+            try {
+                bytes = ((String) object).getBytes("utf-8");
+            } catch(UnsupportedEncodingException e) {
+            }
+        }
+        return bytes != null ? new BytesBlob(bytes, "utf-8") : null;
+    }
+}
---
+++ framework/src/org/wikiup/database/orm/imp/dialect/translator/TimestampFieldFilter.java
@@ -0,0 +1,18 @@
+package org.wikiup.database.orm.imp.dialect.translator;
+
+import org.wikiup.core.inf.Translator;
+import org.wikiup.core.util.Interfaces;
+import org.wikiup.core.util.ValueUtil;
+
+import java.sql.Timestamp;
+
+public class TimestampFieldFilter implements Translator<Object, Timestamp> {
+    public Timestamp translate(Object object) {
+        long time = -1;
+        if(ValueUtil.isInteger(object.toString()))
+            time = ValueUtil.toLong(object.toString(), 0);
+        else if(object instanceof java.util.Date)
+            time = ((java.util.Date) object).getTime();
+        return time != -1 ? new Timestamp(time) : Interfaces.cast(Timestamp.class, object);
+    }
+}
