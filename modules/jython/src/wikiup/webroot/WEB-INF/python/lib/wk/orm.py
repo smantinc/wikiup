70dc8a6418b4fed054315b39e9a51f48d699f7f4
jsjiang <jsjiang@tencent.com>
2015-01-23 17:20:45 +0800

    --------

--- modules/jython/src/wikiup/webroot/WEB-INF/python/lib/wk/orm.py
+++ modules/jython/src/wikiup/webroot/WEB-INF/python/lib/wk/orm.py
@@ -4,15 +4,22 @@
 from org.wikiup.database.orm import WikiupEntityManager
 from org.wikiup.modules.jython.orm import PythonEntity
 
-def getEntity(name, ctx = None):
-	return PythonEntity(_getEntity(name, ctx))
 
-def query(name, condition = {} ,ctx = None):
-	ent = _getEntity(name, ctx)
-	for k in condition:
-		ent.set(k, condition[k])
-	ent.select()
-	return PythonEntity(ent)
+def getEntity(name, condition=None, ctx=None):
+    return PythonEntity(_getEntity(name, condition, ctx))
 
-def _getEntity(name, ctx):
-	return ctx.getEntity(name) if ctx is not None else WikiupEntityManager.getInstance().getEntity(name)
+
+def query(name, relation, condition=None ,ctx=None):
+    entity = _getEntity(name, condition, ctx)
+    relatives = entity.getRelatives(relation, None)
+    props = [str(i.getName()) for i in relatives.getAttributes()]
+    return [dict([(j, str(i.getAttribute(j))) for j in props]) for i in relatives.getChildren()]
+
+
+def _getEntity(name, condition=None, ctx=None):
+    entity = (ctx or WikiupEntityManager.getInstance()).getEntity(name)
+    if condition:
+        for k in condition:
+            entity.set(k, condition[k])
+        entity.select()
+    return entity
