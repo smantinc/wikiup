6cbd06a298a9e5e4c5516ad09e87439ff6739edd
jsjiang <jsjiang@tencent.com>
2015-01-23 14:28:02 +0800

    --------

--- modules/spring/src/org/wikiup/modules/spring/beans/SpringBeanFactory.java
+++ modules/spring/src/org/wikiup/modules/spring/beans/SpringBeanFactory.java
@@ -2,12 +2,13 @@ package org.wikiup.modules.spring.beans;
 
 import org.wikiup.core.Wikiup;
 import org.wikiup.core.inf.BeanContainer;
+import org.wikiup.core.inf.Factory;
 import org.wikiup.core.inf.ext.ModelFactory;
 import org.wikiup.modules.spring.WikiupSpringDynamicSingleton;
 
 import java.util.Iterator;
 
-public class SpringBeanFactory implements ModelFactory, Iterable<String> {
+public class SpringBeanFactory implements ModelFactory, Iterable<String>, Factory<BeanContainer, String> {
     private WikiupSpringDynamicSingleton spring = Wikiup.getModel(WikiupSpringDynamicSingleton.class);
 
     public BeanContainer get(String name) {
@@ -16,5 +17,10 @@ public class SpringBeanFactory implements ModelFactory, Iterable<String> {
 
     public Iterator<String> iterator() {
         return spring.iterator();
+    }
+
+    @Override
+    public BeanContainer build(String name) {
+        return spring.get(name);
     }
 }
