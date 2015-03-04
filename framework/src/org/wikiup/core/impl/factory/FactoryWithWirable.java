98b32956e0fa0f805eb1310982211c7a14be3ba3
jsjiang <jsjiang@tencent.com>
2015-03-04 19:21:51 +0800

    --------

--- framework/src/org/wikiup/core/impl/factory/FactoryWithWirable.java
+++ framework/src/org/wikiup/core/impl/factory/FactoryWithWirable.java
@@ -1,16 +1,21 @@
 package org.wikiup.core.impl.factory;
 
 import org.wikiup.core.impl.cl.ClassDictionaryImpl;
+import org.wikiup.core.inf.Decorator;
 import org.wikiup.core.inf.Document;
 import org.wikiup.core.inf.Factory;
 import org.wikiup.core.inf.ext.Wirable;
 import org.wikiup.core.util.Interfaces;
 
 public class FactoryWithWirable<T> implements Factory<T> {
-    private final FactoryImpl<?> factory;
+    private final Factory<?> factory;
 
     public FactoryWithWirable() {
         this.factory = new FactoryImpl<Object>(new ClassDictionaryImpl());
+    }
+
+    public FactoryWithWirable(Factory<T> factory) {
+        this.factory = factory;
     }
 
     @Override
@@ -20,5 +25,12 @@ public class FactoryWithWirable<T> implements Factory<T> {
         if(wirable != null)
             bean = wirable.wire(desc);
         return (T) bean;
+    }
+
+    public static final class DECORATOR<T> implements Decorator<Factory<T>> {
+        @Override
+        public Factory<T> decorate(Factory<T> factory, Document desc) {
+            return new FactoryWithWirable<T>(factory);
+        }
     }
 }
