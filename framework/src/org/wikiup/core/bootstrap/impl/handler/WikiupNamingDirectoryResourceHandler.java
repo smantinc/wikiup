9405d68d3e15940c70d4a76b0d4bfcb23b07fff9
jsjiang <jsjiang@tencent.com>
2015-03-05 18:04:56 +0800

    --------

--- framework/src/org/wikiup/core/bootstrap/impl/handler/WikiupNamingDirectoryResourceHandler.java
+++ framework/src/org/wikiup/core/bootstrap/impl/handler/WikiupNamingDirectoryResourceHandler.java
@@ -1,5 +1,10 @@
 package org.wikiup.core.bootstrap.impl.handler;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 import org.wikiup.core.Wikiup;
 import org.wikiup.core.bean.WikiupNamingDirectory;
 import org.wikiup.core.bean.WikiupPluginManager;
@@ -11,6 +16,7 @@ import org.wikiup.core.inf.ext.Resource;
 import org.wikiup.core.util.Assert;
 import org.wikiup.core.util.Documents;
 import org.wikiup.core.util.Interfaces;
+import org.wikiup.core.util.StringUtil;
 
 public class WikiupNamingDirectoryResourceHandler extends DirectoryDocumentResourceHandler {
     private static final String WK_DESC_NAME = "desc";
@@ -20,9 +26,19 @@ public class WikiupNamingDirectoryResourceHandler extends DirectoryDocumentResou
     private Document container = Documents.create("root");
     private WikiupNamingDirectory wnd = WikiupNamingDirectory.getInstance();
 
+    private HashMap<String, DirectoryList> directories = new HashMap<String, DirectoryList>();
+
     @Override
     public void finish() {
+/*
         loadDirectory(container);
+/*/
+        ArrayList<DirectoryList> sorted = new ArrayList<DirectoryList>(directories.values());
+        Collections.sort(sorted);
+
+        for(DirectoryList directoryList : sorted)
+            directoryList.create(wnd);
+/**/
     }
 
     @Override
@@ -33,6 +49,12 @@ public class WikiupNamingDirectoryResourceHandler extends DirectoryDocumentResou
         Documents.setAttributeValue(item, WK_DESC_NAME, attr != null ? (virtual ? new MergedDocument(doc, attr) : new MergedDocument(attr, doc)) : doc);
         Documents.setAttributeValue(item, WK_PATH_NAME, path);
         Documents.setAttributeValue(item, WK_RESOURCE_NAME, resource);
+
+        String key = StringUtil.join(path, "/");
+        DirectoryList directoryList = directories.get(key);
+        if(directoryList == null)
+            directories.put(key, directoryList = new DirectoryList(path));
+        directoryList.directories.add(new Directory(doc, resource));
     }
 
     private void loadDirectory(Document doc) {
@@ -77,5 +99,61 @@ public class WikiupNamingDirectoryResourceHandler extends DirectoryDocumentResou
             Interfaces.initialize(container, doc);
         else
             wnd.create(path, doc, path.length);
+    }
+
+    private static class DirectoryList implements Comparable<DirectoryList> {
+        public String[] path;
+        public LinkedList<Directory> directories = new LinkedList<Directory>();
+
+        public DirectoryList(String[] path) {
+            this.path = path;
+        }
+
+        public void create(WikiupNamingDirectory wnd) {
+            Object directory = wnd.get(path);
+            Document desc = getDocument();
+            if(Wikiup.getCsidAttribute(desc, null) != null) {
+                Object bean = Wikiup.getInstance().getBean(Object.class, desc);
+                Interfaces.initialize(bean, desc);
+                wnd.set(path, bean);
+            } else if(directory != null)
+                Interfaces.initialize(directory, desc);
+            else
+                create(wnd, desc);
+        }
+
+        private void create(WikiupNamingDirectory wnd, Document doc) {
+            Object container = wnd.get(path);
+            if(container != null)
+                Interfaces.initialize(container, doc);
+            else
+                wnd.create(path, doc, path.length);
+        }
+
+        @Override
+        public int compareTo(DirectoryList o) {
+            return path.length - o.path.length;
+        }
+        
+        private Document getDocument() {
+            Document doc = null;
+            for(Directory directory : directories) {
+                if(doc == null)
+                    doc = directory.document;
+                else if(!Documents.getAttributeBooleanValue(doc, "virtual", false))
+                    doc = new MergedDocument(directory.document, doc);
+            }
+            return doc;
+        }
+    }
+
+    private static class Directory {
+        public Document document;
+        public Resource resource;
+
+        public Directory(Document document, Resource resource) {
+            this.document = document;
+            this.resource = resource;
+        }
     }
 }
