09156110f2d89ea0abe9daad774ead16d1b847f0
jsjiang <jsjiang@tencent.com>
2015-03-06 16:40:20 +0800

    --------

--- framework/src/org/wikiup/servlet/inf/ProcessorModelContainer.java
+++ framework/src/org/wikiup/servlet/inf/ProcessorModelContainer.java
@@ -1,8 +1,9 @@
 package org.wikiup.servlet.inf;
 
-import org.wikiup.core.inf.Dictionary;
 import org.wikiup.core.inf.BeanContainer;
+import org.wikiup.core.inf.Dictionary;
 
+@Deprecated
 public interface ProcessorModelContainer {
     public BeanContainer getModelContainer(String name, Dictionary<?> params);
 }
